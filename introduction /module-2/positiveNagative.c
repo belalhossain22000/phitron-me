e0e13be8c8fccd15093a4e37b890f3f255ca2dc8
belalhossain22000 <belalhossain22000@gmail.com>
2025-04-09 00:40:28 +0600

    check even odd

---
+++ introduction /module-2/positiveNagative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a;
+
+    scanf("%d", &a);
+
+    if (a  > 0)
+    {
+        printf("the number is positive =%d", a);
+    }
+    else
+    {
+        printf("the number is negative %d", a);
+    }
+
+    return 0;
+}
