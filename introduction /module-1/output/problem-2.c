c049ef35d9f08b1147097f0d37cda1014061a00d
belalhossain22000 <belalhossain22000@gmail.com>
2025-04-08 23:35:57 +0600

    done arithmatic operation using c

---
+++ introduction /module-1/output/problem-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+   
+    printf("Second number = %d\n", b);
+    printf("First number = %d\n", a);
+
+    return 0;
+}
---
+++ introduction /module-2/arithmetic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 2;
+
+    float b = 5;
+
+    // int sum = a + b;
+    // printf("summation=%d\n", sum);
+    // int sub= a-b;
+    // printf("substraction=%d\n",sub);
+    // int mul = a*b;
+    // printf("multiplication=%d\n",mul);
+
+    float div=b/a;
+    printf("division=%f",div);
+    return 0;
+}
