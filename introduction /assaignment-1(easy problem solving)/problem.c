356db964f73f3c497a3a68884a2fb0f1db4e67d0
belalhossain22000 <belalhossain22000@gmail.com>
2025-04-11 20:47:45 +0600

    Add initial implementation of problem-solving exercises in C
    - Introduced multiple problems as commented code snippets in problem.c
    - Included examples for basic I/O operations, arithmetic calculations, conditional statements, and loops.
    - Problems cover topics such as multiplication, divisibility checks, and conditional outputs based on user input.

---
+++ introduction /assaignment-1(easy problem solving)/problem.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+// //problem -1
+// int main (){
+//     printf("Hello, world! I am learning C programming language. ^_^\n");
+//     printf("Programming is fun and challenging. /\\/\\/\\ \n");
+//     printf("I want to give my 100%% dedication to learn!\tI will succeed one day.\n");
+//     return 0;
+// }
+
+// // problem -2
+// int main()
+// {
+//     long long int A;
+//     long long int B;
+
+//     scanf("%lld", &A);
+
+//     scanf("%lld", &B);
+
+//     long long int mul = A * B;
+
+//     printf("%lld", mul);
+
+//     return 0;
+// }
+
+// problem -3
+// int main()
+// {
+//     long long int N;
+
+//     scanf("%lld", &N);
+
+//     if(N%3==0){
+//         printf("YES");
+//     }else{
+//         printf("NO");
+//     }
+
+//     return 0;
+// }
+
+// // problem -4
+// int main()
+// {
+//     int N;
+
+//     scanf("%d", &N);
+
+//     for (int i=0; i <= N; i++)
+//         if (i % 3 == 0 && i % 7 == 0)
+      
+
+//         {
+//             if(i==0){
+//                 continue;
+//             }
+//             printf("%d\n", i);
+//         }
+
+//     return 0;
+// }
+
+
+// problem -5
+// int main()
+// {
+//     int N;
+
+//     scanf("%d", &N);
+
+
+//     if(N >1000){
+//         printf("I will buy Punjabi\n");
+//         if(N-1000>=500){
+//             printf("I will buy new shoes\n");
+//             printf("Alisa will buy new shoes\n");
+           
+//         }
+//     }else{
+//         printf("Bad luck!\n");
+//     }
+
+//     return 0;
+// }
