8d69c40d168d98703d6f1114f3076101dccdb642
belalhossain22000 <belalhossain22000@gmail.com>
2025-04-13 23:01:21 +0600

    solve mcq problem

--- introduction /module-5/for-you-with-condition.c
+++ introduction /module-5/for-you-with-condition.c
@@ -138,3 +138,27 @@
     
 //     return 0;
 // }
+
+
+//mcq test
+#include <stdio.h>
+
+int main() {
+    
+    
+    int x = 0;
+    if (x == 1)
+    {
+        if (x >= 0)
+        {
+            printf("true\n");
+        }
+        else
+        {
+            printf("false\n");
+        }
+    }
+   
+    
+    return 0;
+}
