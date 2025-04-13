c5ade0096c6e0be3115632529e0464fe2f1696a0
belalhossain22000 <belalhossain22000@gmail.com>
2025-04-14 01:05:49 +0600

    solved practice more 2  problem

--- introduction /module-5/for-you-with-condition.c
+++ introduction /module-5/for-you-with-condition.c
@@ -1,13 +1,11 @@
 // #include <stdio.h>
 
-
-//problem-1
-
+// problem-1
 
 // #include <stdio.h>
 
 // int main() {
-    
+
 //     int a;
 //     int b;
 
@@ -17,12 +15,11 @@
 //     }else{
 //         printf("No");
 //     }
-    
+
 //     return 0;
 // }
 
-
-//problem-2
+// problem-2
 
 // #include <stdio.h>
 // int main() {
@@ -37,8 +34,7 @@
 //     }else{
 //         printf("No Multiples");
 //     }
-    
-    
+
 //     return 0;
 // }
 
@@ -49,10 +45,10 @@
 // #include <stdio.h>
 
 // int main() {
-    
+
 //     int a;
-//     scanf("%d",&a);   
-//     int div=a/1000; 
+//     scanf("%d",&a);
+//     int div=a/1000;
 //     if(div%2==0){
 //         printf("EVEN");
 //     }else{
@@ -67,7 +63,7 @@
 // #include <stdio.h>
 
 // int main() {
-    
+
 //     char ch;
 //     scanf("%c",&ch);
 //     if(ch>='a' && ch<='z')
@@ -80,12 +76,12 @@
 //         ch=ch+32;
 //         printf("%c",ch);
 //     }
-    
+
 //     return 0;
 // }
 
-//M. Capital or Small or Digit
-// problem-5
+// M. Capital or Small or Digit
+//  problem-5
 
 // #include <stdio.h>
 // int main() {
@@ -103,13 +99,12 @@
 //             printf("IS CAPITAL");
 //         }
 //     };
-    
-    
+
 //     return 0;
 // }
 
-//K. Max and Min 
-//problem-6
+// K. Max and Min
+// problem-6
 
 // #include <stdio.h>
 
@@ -134,31 +129,79 @@
 //     }else if(c>=a && c>=b){
 //         printf(" %d", c);
 //     }
-     
-    
+
 //     return 0;
 // }
 
+// mcq test
+//  #include <stdio.h>
 
-//mcq test
+// int main() {
+
+//     int x = 0;
+//     if (x == 1)
+//     {
+//         if (x >= 0)
+//         {
+//             printf("true\n");
+//         }
+//         else
+//         {
+//             printf("false\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
+// practice problem-1
+// link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+// wrong code mistake here when multiply then not taking long long
+// #include <stdio.h>
+
+// int main() {
+//     int a;
+//     int b;
+//     scanf("%d %d",&a,&b);
+
+//     int sum=a+b;
+//     int mul =a*b;
+//     int sub =a-b;
+//     printf("%d + %d = %d\n",a, b, sum);
+//     printf("%d * %d = %d\n",a, b, mul);
+//     printf("%d - %d = %d\n",a, b, sub);
+
+//     return 0;
+// }
+
+// // correct code
+// #include <stdio.h>
+
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+
+//     int sum = a + b;
+//     long long mul = 1LL * a * b;
+//     int sub = a - b;
+
+//     printf("%d + %d = %d\n", a, b, sum);
+//     printf("%d * %d = %lld\n", a, b, mul);
+//     printf("%d - %d = %d\n", a, b, sub);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main() {
-    
-    
-    int x = 0;
-    if (x == 1)
-    {
-        if (x >= 0)
-        {
-            printf("true\n");
-        }
-        else
-        {
-            printf("false\n");
-        }
-    }
-   
-    
+int main()
+{
+
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    long long x = 1LL * a * b - 1LL * c * d;
+    printf("Difference = %lld", x);
+
     return 0;
 }
